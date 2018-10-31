7b2569165eab682ca7b8094b8e5dce1f22963873
RobbyRed98 <roth-robin@web.de>
2018-11-01 00:31:04 +0100

    changed to mariadb

--- src/server.js
+++ src/server.js
@@ -1,46 +1,76 @@
-var express    = require('express');        
-var app        = express();
-var fs         = require("fs");
-var config     = JSON.parse(fs.readFileSync("config.json"));                
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var config = JSON.parse(fs.readFileSync("config.json"));
 var bodyParser = require('body-parser');
-var mysql      = require('mysql');
-var cors       = require('cors')
+var mariadb = require('mariadb');
+var cors = require('cors')
 var serverConfig = config.databaseConfig;
-var connection = mysql.createConnection(serverConfig);
- 
-connection.connect();
+var pool = mariadb.createPool(serverConfig);
 
 var cors = require('cors');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-var port = process.env.PORT || 5000;       
+var port = process.env.PORT || 5000;
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    connection.query("Select * from series ORDER BY name", (error,results,fields)=>{
+router.get('/', async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const results = await conn.query("Select * from series ORDER BY name");
         res.json(results);
-    });   
+        console.log("Sent", results.length, "items of all types.");
+    } catch(err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
 });
 
-router.get('/type/:type', function(req, res) {
-    connection.query("SELECT * FROM series WHERE typ = ? ORDER BY name", [req.params.type] ,(error,results,fields)=>{
+router.get('/type/:type', async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const results = await conn.query("SELECT * FROM series WHERE typ = ? ORDER BY name", [req.params.type]);
         res.json(results);
-    });
+        console.log("Sent", results.length, "items of type", req.params.type, ".");
+    } catch(err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
 });
 
-router.get('/genre/:genre', function(req, res) {
-    connection.query("SELECT * FROM series WHERE genre = ? ORDER BY name", [req.params.genre] ,(error,results,fields)=>{
+router.get('/genre/:genre', async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const results = await conn.query("SELECT * FROM series WHERE genre = ? ORDER BY name", [req.params.genre]);
         res.json(results);
-    });
-});  
+        console.log("Sent", results.length, "items of genre", req.params.genre, ".");
+    } catch(err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
+});
 
-router.get('/:type/:genre', function(req, res) {
-    connection.query("SELECT * FROM series WHERE typ = ? AND genre = ? ORDER BY name", [req.params.type, req.params.genre], (error,results,fields)=>{
+router.get('/:type/:genre', async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const results = await conn.query("SELECT * FROM series WHERE typ = ? AND genre = ? ORDER BY name", [req.params.type, req.params.genre]);
         res.json(results);
-    });
+        console.log("Sent", results.length, "items of type", req.params.type, "and genre", req.params.genre, ".");
+    } catch(err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
 });
 
 app.use('/media', router);
